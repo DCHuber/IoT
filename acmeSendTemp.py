d71b535fc4b8edb2717ccc776e13f64ca4fb3ef9
Dan Huber <dhuber@gisinc.com>
2017-11-20 19:16:44 -0700

    Heavily updated logic
    Moved from a looping service to a scheduled task to limit footprint

--- acmeSendTemp.py
+++ acmeSendTemp.py
@@ -1,15 +1,28 @@
+#!/usr/bin/python3
+
 from w1thermsensor import W1ThermSensor
-import time, requests, json
+import requests, json
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+#Setup logging information
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = TimedRotatingFileHandler('/home/pi/Projects/acmeFactory/logs/temp.log', when='midnight', backupCount=30, utc=False)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s,%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 
 sensor = W1ThermSensor()
-rmTemp = sensor.get_temperature(W1ThermSensor.DEGREES_F)
 tempID = sensor.id
 
-postURL = 'http://fedapps.gisinc.com/acme/add_message'
+postURL = ''
 
-start = time.time()
-
-end = time.time()
-if ((end - start) > 300):
-	requests.post(postURL, json={'id': tempID, 'tempF':  rmTemp, 'lat': 39.0108867, 'lon': -105.0510984})
-	start = time.time()
+try:
+	rmTemp = sensor.get_temperature(W1ThermSensor.DEGREES_F)
+	requests.post(postURL, json={'definition': 'acmeTemps', 'id': tempID, 'tempF':  rmTemp, 'lat': 39.010793, 'lon': -105.050999})
+	logger.info('%s, %f', tempID, rmTemp)
+except:
+	logger.debug ("Error publishing temp")
